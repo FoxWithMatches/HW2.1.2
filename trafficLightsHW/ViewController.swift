8a0c7ec03d683b7ce26d075e8018bc19fe1525b6
Alisa Ts <freiheiit92@mail.ru>
2021-10-20 00:37:35 +0300

    hw

--- trafficLightsHW/ViewController.swift
+++ trafficLightsHW/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
+enum Colors {
+    case red
+    case yellow
+    case green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var textButton: UIButton!
+    
+    private var colors = Colors.red
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.alpha = 0.3
+        redLight.layer.masksToBounds = true
+        redLight.layer.cornerRadius = redLight.frame.height / 2
+        
+        
+        yellowLight.alpha = 0.3
+        yellowLight.layer.masksToBounds = true
+        yellowLight.layer.cornerRadius = yellowLight.frame.height / 2
+        
+        greenLight.alpha = 0.3
+        greenLight.layer.masksToBounds = true
+        greenLight.layer.cornerRadius = greenLight.frame.height / 2
     }
 
-
+   
+    @IBAction func toggleButton() {
+        textButton.setTitle("Next", for: .normal)
+        
+        switch colors {
+        case .red:
+            greenLight.alpha = 0.3
+            redLight.alpha  = 1
+            colors = .yellow
+        case .yellow:
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+            colors = .green
+        case .green:
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+            colors = .red
+        }
+    }
+    
+   
 }
 
